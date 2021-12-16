fe02da935a93f7f9f775688c82ce795451898d04
smectaz <90684643+smectaz@users.noreply.github.com>
2021-12-16 17:53:24 +0100

    create challenge-8

--- src/app/search-movies/search-movies.component.ts
+++ src/app/search-movies/search-movies.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-search-movies',
   templateUrl: './search-movies.component.html',
-  styleUrls: ['./search-movies.component.scss']
+  styleUrls: ['./search-movies.component.scss'],
 })
 export class SearchMoviesComponent implements OnInit {
+  registerForm!: FormGroup;
+  submitted = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      id: ['', Validators.required],
+      titre: ['', Validators.required],
+      type: ['', Validators.required],
+      date: ['', [Validators.required, Validators.min(1900)]],
+      fiche: ['', Validators.required],
+    });
   }
 
+  // convenience getter for easy access to form fields
+  get film() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    // display form values on success
+    alert(
+      'SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4)
+    );
+  }
+
+  onEfface() {
+    this.submitted = false;
+    this.registerForm.reset();
+  }
 }
